01759f98d6a7c01a4f6cdad65fe5684e3b9a9510
Pedro Sperotto <114098634+Lecrepus17@users.noreply.github.com>
2023-03-24 11:07:10 -0300

    aa

--- 0203/documentos_novo.php
+++ 0203/documentos_novo.php
@@ -1,5 +1,10 @@
 <?php
     require('twig_carregar.php');
+
+    if($_SERVER['REQUEST_METHOD'] == 'POST' && !$_FILES['arquivo']['error']){
+        move_uploaded_file($_FILES['arquivo']['tmp_name'], 'uploads/' . $_FILES['arquivo']['name']);
+    }
+
     echo"<pre>";
     var_dump($_POST);
 
